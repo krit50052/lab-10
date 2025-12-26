633a4a84cc105e3b50ca89fd46074da7cd1c8172
krit <krittapasboonruang@gmail.com>
2025-12-26 20:05:23 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    
+    double sum = 0, sumSquare = 0;
+    int count = 0;
+    double score;
+    
+    while(source >> score){
+        sum += score;
+        sumSquare += score * score;
+        count++;
+    }
+    
+    source.close();
+    
+    double mean = sum / count;
+
+    double variance = (sumSquare / count) - (mean * mean);
+    double stdDev = sqrt(variance);
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << stdDev << "\n";
+    
+    return 0;
+}
